379e4e07a68cd40ef65ed2854c532cae72642d15
Sai Krishna <chsaikrishna2022@gmail.com>
2024-11-14 14:52:31 +0530

    Created Shopping App

---
+++ src/test/java/com/ecom/Shopping_App/ShoppingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecom.Shopping_App;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
